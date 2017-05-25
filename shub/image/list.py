c47212d65db0af2eb935d0431232c0b9397f076b
Viktor Shlapakov <vshlapakov@gmail.com>
2017-05-25 12:49:40 +0300

    Initial changes

--- shub/image/list.py
+++ shub/image/list.py
@@ -3,9 +3,10 @@ import json
 import click
 import requests
 from six import binary_type
+from six import string_types
 from six.moves.urllib.parse import urljoin
 
-from shub import exceptions as shub_exceptions
+from shub.exceptions import ShubException
 from shub.config import load_shub_config, list_targets_callback
 from shub.image import utils
 
@@ -62,17 +63,27 @@ def list_cmd_full(target, silent, version):
 def list_cmd(image_name, project, endpoint, apikey):
     """Short version of list cmd to use with deploy cmd."""
     settings = _get_project_settings(project, endpoint, apikey)
-
-    # Run a local docker container to run list-spiders cmd
-    status_code, logs = _run_list_cmd(project, image_name, settings)
-    if status_code != 0:
+    environment = {'JOB_SETTINGS': json.dumps(settings)}
+    exit_code, logs = _run_cmd_in_docker_container(
+            image_name, 'shub-image-info', environment)
+    if exit_code == 0:
+        return _extract_metadata_from_image_info_output(logs)
+    # shub-image-info command not found, fallback to list-spiders
+    elif exit_code == 127:
+        # FIXME we should pass some value for SCRAPY_PROJECT_ID anyway
+        # to handle `scrapy list` cmd properly via sh_scrapy entrypoint
+        environment['SCRAPY_PROJECT_ID'] = str(project) if project else ''
+        exit_code, logs = _run_cmd_in_docker_container(
+            image_name, 'list-spiders', environment)
+        if exit_code != 0:
+            click.echo(logs)
+            raise ShubException('Container with list cmd exited with code %s' % exit_code)
+        logs = logs.decode('utf-8') if isinstance(logs, binary_type) else logs
+        return {'spiders': utils.valid_spiders(logs)}
+    else:
         click.echo(logs)
-        raise shub_exceptions.ShubException(
-            'Container with list cmd exited with code %s' % status_code)
-
-    spiders = utils.valid_spiders(
-        logs.decode('utf-8') if isinstance(logs, binary_type) else logs)
-    return spiders
+        raise ShubException(
+            'Container with shub-image-info cmd exited with code %s' % exit_code)
 
 
 def _get_project_settings(project, endpoint, apikey):
@@ -89,27 +100,39 @@ def _get_project_settings(project, endpoint, apikey):
     return {k: v for k, v in req.json().items() if k in SETTING_TYPES}
 
 
-def _run_list_cmd(project, image_name, project_settings):
-    """Run `scrapy list` command inside the image container."""
-
+def _run_cmd_in_docker_container(image_name, command, environment):
+    """Run a command inside the image container."""
     client = utils.get_docker_client()
-    # FIXME we should pass some value for SCRAPY_PROJECT_ID anyway
-    # to handle `scrapy list` cmd properly via sh_scrapy entrypoint
-    project = str(project) if project else ''
-    job_settings = json.dumps(project_settings)
     container = client.create_container(
         image=image_name,
-        command=['list-spiders'],
-        environment={'SCRAPY_PROJECT_ID': project,
-                     'JOB_SETTINGS': job_settings})
+        command=[command],
+        environment=environment,
+    )
     if 'Id' not in container:
-        raise shub_exceptions.ShubException(
-            "Create container error:\n %s" % container)
-
+        raise ShubException("Create container error:\n %s" % container)
     client.start(container)
     statuscode = client.wait(container=container['Id'])
-
     return statuscode, client.logs(
             container=container['Id'],
             stdout=True, stderr=True if statuscode else False,
             stream=False, timestamps=False)
+
+
+def _extract_metadata_from_image_info_output(output):
+        try:
+            metadata = json.loads(output)
+        except ValueError:
+            raise ShubException('shub-image-info: output is not a valid JSON: {}'
+                                .format(output))
+        spiders_data = metadata.get('spiders', {})
+        if not isinstance(spiders_data, dict):
+            raise ShubException('shub-image-info: spiders section must be '
+                                'a dictionary, got {}'.format(spiders_data))
+        elif not all(name and isinstance(name, string_types)
+                     for name in spiders_data.keys()):
+            raise ShubException('shub-image-info: spiders section should '
+                               'contain non-empty string spider names')
+        spiders, scripts = [], []
+        for name in spiders_data:
+            spiders.append(name) if name.startswith('py:') else scripts.append(name)
+        return {'spiders': spiders, 'scripts': scripts}
